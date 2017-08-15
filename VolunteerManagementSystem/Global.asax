d4b56e589a69dfcaeded16ef22c84c77f8446344
MatthewRamsay <Matthew.t.ramsay@gmail.com>
2017-08-15 10:29:34 -0400

    first commit

---
+++ VolunteerManagementSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VolunteerManagementSystem.MvcApplication" Language="C#" %>
---
+++ VolunteerManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VolunteerManagementSystem.Startup))]
+namespace VolunteerManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
